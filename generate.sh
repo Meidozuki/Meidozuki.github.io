37e329db37e81e379b97471e93661613990ef853
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2023-01-12 22:07:03 +0800

    add Graph

--- generate.sh
+++ generate.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-fnames=$(ls */img/*.dot)
+fnames="$(ls */img/*.dot)"
 
-for name in $fnames; do
-  echo $name
-  dot -O -Tjpg $name
+for name in "$fnames" ; do
+    echo "$name"
+    dot -O -Tjpg "$name"
 done
